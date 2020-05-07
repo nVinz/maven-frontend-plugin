23c4f7ba4892d090f789ff0cdf8f6c3163c10c69
Arkady Shelepukhin <arkon.f2@gmail.com>
2020-05-08 00:11:18 +0300

    initial commit

---
+++ src/main/java/my/nvinz/mavenfrontend/MavenfFontendApplication.java
@@ -0,0 +1,13 @@
+package my.nvinz.mavenfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenfFontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenfFontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/nvinz/mavenfrontend/MavenfFontendApplicationTests.java
@@ -0,0 +1,13 @@
+package my.nvinz.mavenfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenfFontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
